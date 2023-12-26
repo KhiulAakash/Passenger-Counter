879e3fbd6f076aa271d99890066862aa71d3afa1
KhiulAakash <khiulaakash123@gmail.com>
2023-12-26 11:36:20 +0545

    Final commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// Selecting elements
+const countEl = document.querySelector("#count-el");
+const incrementBtn = document.querySelector("#increment-btn");
+const saveBtn = document.querySelector("#save-btn");
+const saveEl = document.querySelector(".save #save-el");
+
+let count = 0;
+const saveHistory = []; // Array to store the last 3 counts
+
+// Function to increment count and update count display
+function incrementCount() {
+    count++;
+    countEl.textContent = count;
+}
+
+// Function to save count
+function saveCount() {
+    if (saveHistory.length === 3) {
+        saveHistory.shift(); // Remove the oldest count if there are already 3 counts
+    }
+    saveHistory.push(count); // Add the new count to the history
+
+    // Display the last 3 counts in the save element
+    saveEl.textContent = saveHistory.join(" - ");
+}
+
+// Event listeners
+incrementBtn.addEventListener("click", incrementCount);
+saveBtn.addEventListener("click", saveCount);
